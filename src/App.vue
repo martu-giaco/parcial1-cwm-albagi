cb176b5ce5bd2a47b791693b94abf2d28ccfe469
martu-giaco <martina.giacomini@davinci.edu.ar>
2025-10-04 12:53:41 -0300

    hice un par de bobinadas de estilos para no caer en demecia, habría que conseguir una font para la marca dios

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,11 @@
 </script>
 
 <template>
-    <nav class="flex items-center p-4 justify-between rounded-b-lg shadow-lg">
-        <p>Kälm</p>
+    <nav class="flex items-center p-4 justify-between rounded-b-2xl shadow-md">
+        <div class="flex items-center">
+            <img src="/public/k-dark.svg" alt="Kälm logo dark" class="h-8">
+            <p class="text-lg font-bold ml-3 line-clamp-1 text-(--color-dark-kalm)">Kälm</p>
+        </div>
         <ul class="flex">
             <li class="mr-2 px-2 py-1"><router-link to="/">Inicio</router-link></li>
             <li class="mr-2 px-2 py-1"><router-link to="/publicaciones">Publicaciones</router-link></li>
@@ -20,7 +23,7 @@
     <main>
         <RouterView />
     </main>
-    <footer class="flex items-end p-4 justify-between bg-(--color-primary-kalm) rounded-t-lg text-white">
+    <footer class="flex items-end px-4 py-10 justify-between bg-(--color-dark-kalm) rounded-t-2xl text-white">
         <div>
                 <ul>
                     <li><router-link to="/">Inicio</router-link></li>
@@ -30,10 +33,8 @@
                 </ul>
             <p class="mt-3">© 2025 Kälm. All rights reserved.</p>
         </div>
-        <div class="text-end">
-            <picture>
-                <img src="" alt="">
-            </picture>
+        <div class="text-end flex flex-col items-end flex-wrap">
+                <img class="h-20" src="/public/K.svg" alt="Kälm logo">
             <p class="mt-3">Escuela Da Vinci - DWM4AP</p>
             <p>Clientes Web Mobile, Parcial 1. Alvarez, Barrientos, Giacomini</p>
         </div>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -8,10 +8,10 @@ import AppH1 from '../components/AppH1.vue';
 </script>
 
 <template>
-    <header class="m-4 p-9 mb-8 rounded-xl text-white flex justify-between items-center bg-[url(../../public/header-image.png)] h-[30vw]">
-        <div class="w-50">
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-            <p class="mr-2 px-2 py-1 border-(--color-primary-kalm) rounded-md font-bold bg-(--color-primary-kalm) text-white w-20 text-center"><router-link to="/signin">Unirse</router-link></p>
+    <header class="bg-cover m-4 p-9 mb-8 rounded-xl text-white flex justify-between items-center bg-[url(../../public/header-image.png)] h-[30vw]">
+        <div class="w-160 ml-5">
+            <h1 class="font-bold text-5xl mb-3 text-(--color-dark-kalm)">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+            <p class="mr-2 px-2 py-1 border-(--color-primary-kalm) rounded-xl bg-(--color-primary-kalm) text-white w-20 text-center"><router-link to="/signin">Unirse</router-link></p>
         </div>
     </header>
 </template>
